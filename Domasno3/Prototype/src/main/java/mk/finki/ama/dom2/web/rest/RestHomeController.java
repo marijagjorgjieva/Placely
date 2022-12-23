10e29f5859dab7443c88fdaa11fb100173fa0227
marijagjorgjieva <mgjorgjieva2013@gmail.com>
2022-12-23 21:22:44 +0100

    Created a new RestHomeController class
    - addded a new .rest
    - added a new post mapping that returns a ResponseEntity<List<Place>> with a json header.

---
+++ Domasno3/Prototype/src/main/java/mk/finki/ama/dom2/web/rest/RestHomeController.java
@@ -0,0 +1,62 @@
+package mk.finki.ama.dom2.web.rest;
+
+import mk.finki.ama.dom2.model.Location;
+import mk.finki.ama.dom2.model.Place;
+import mk.finki.ama.dom2.pipeAndFilter.filterImpl.CategoryFilter;
+import mk.finki.ama.dom2.pipeAndFilter.filterImpl.DistanceFilter;
+import mk.finki.ama.dom2.pipeAndFilter.pipe.Pipeline;
+import mk.finki.ama.dom2.service.PlacesService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:3000") zosto vaka
+@RequestMapping("/api/home")
+public class RestHomeController {
+
+    private final PlacesService placesService;
+
+    public RestHomeController(PlacesService placesService) {
+        this.placesService = placesService;
+    }
+
+    //TODO dali treba nesto drugo i glava boli od ova malce kako e nastrakano messy mi ide hmmmmm
+    @PostMapping("/userPreferences")
+    public ResponseEntity<List<Place>> showResults(
+            @RequestParam String city,
+            @RequestParam List<String> select1,
+            @RequestParam String location1,
+            @RequestParam List<String> select2,
+            @RequestParam String location2)
+            throws IOException, InterruptedException {
+
+        String[]locations1 = location1.split(",");
+        String[]locations2 = location2.split(",");
+        Location l1 = new Location(Double.parseDouble(locations1[0]), Double.parseDouble(locations1[1]));
+        Location l2 = new Location(Double.parseDouble(locations2[0]), Double.parseDouble(locations2[1]));
+
+        Set<String> mutualCategories = new HashSet<>(select1);
+        Set<String> categories2 = new HashSet<>(select2);
+        mutualCategories.retainAll(categories2);
+
+        List<Place> placesToBeReturned = new ArrayList<>();
+        for (String category : mutualCategories) {
+            Pipeline<List<Place>, List<Place>> pipeline1 =
+                    new Pipeline<>(new CategoryFilter(category)).chain(new DistanceFilter(l1, l2));
+            placesToBeReturned.addAll(pipeline1.process(placesService.ListAllByName(city)));
+        }
+
+        return ResponseEntity.ok().header("Content-Type", "application/json").body(placesToBeReturned);
+    }
+}
+
+
