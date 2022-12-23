70e957079195eccf8a1ce7537af13e21ff94b911
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2022-12-23 13:49:04 +0100

    added leaflet map

---
+++ Domasno3/frontend/placely-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MapComponent from './MapComponent.js'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <MapComponent/>
+    </div>
+  );
+}
+
+export default App;
