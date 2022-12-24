4d2140a2153e2bac0fdb144189c30011bae41b14
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2022-12-24 18:54:42 +0100

    working app

--- Domasno3/frontend/placely-frontend/src/App.js
+++ Domasno3/frontend/placely-frontend/src/App.js
@@ -1,19 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MapComponent from './MapComponent.js'
 import InputCard from './InputCard.js'
 
-function App() {
+class App extends React.Component {
+
+
+
+  constructor(props) {  
+    super(props);
+    this.state = { results : {} };
+    this.handleResults = this.handleResults.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  // state = { results : new Array() }
+
+     handleResults (r) {
+        this.setState({results: r});
+        
+    }
+
  
-  return (
-    <div className="App">
-      <header className="app-header">
-        <h1 className='logo'>Placely</h1>
-      </header>
-      <MapComponent/>
-      <InputCard/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="app-header">
+          <h1 className='logo'>Placely</h1>
+        </header>
+        <MapComponent results = {this.state.results}/>
+        <InputCard onGetResults = {this.handleResults}/>
+      </div>
+    );
+  }
 }
 
 export default App;
