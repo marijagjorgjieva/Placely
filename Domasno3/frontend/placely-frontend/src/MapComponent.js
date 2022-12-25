b593dcf35d939a3a8977e53b85ece29a1bb91d2a
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2022-12-25 12:36:57 +0100

    made map move to specified city

--- Domasno3/frontend/placely-frontend/src/MapComponent.js
+++ Domasno3/frontend/placely-frontend/src/MapComponent.js
@@ -14,6 +14,13 @@ const icon = L.icon({ iconUrl: "/marker-icon.png" });
 //     shadowUrl: require('leaflet/dist/images/marker-shadow.png').default
 // });
 
+function MyComponent(props) {
+    const map = useMap()
+    if (props.lat !== undefined && props.lon !== undefined)
+        map.setView([props.lat,props.lon], 14)
+    return null
+  }
+
 class MapComponent extends React.Component {
 
     constructor(props) {
@@ -33,14 +40,21 @@ class MapComponent extends React.Component {
         if (results !== prevProps.results) {
           this.setState({ res: results.res });
         }
-      }
+        // var map = useMap();
+        // const res  = this.state.res !== undefined ? this.state.res : [];
+        // if (res.length > 0) {
+        //     map.panTo([res[0].location.latitude, res[0].location.longitude])
+        // }
+    }
     render() {
         const { res = [] } = this.state;
+        const first = res.length > 0 ? res[0] : {location: {latitude: 41.996, longitude: 21.4310}};
     
         return (
             
             <div className='map'>
-            <MapContainer center={[41.996, 21.4310]} zoom={14} scrollWheelZoom={false}>
+            <MapContainer center={[41.996, 21.4310]} zoom={14} scrollWheelZoom={true}>
+                <MyComponent lat={first.location.latitude} lon={first.location.longitude}/>
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                 {
