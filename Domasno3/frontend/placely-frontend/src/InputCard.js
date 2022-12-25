bd9bc510fa3f9e63d79f7dab66f8344766b27461
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2022-12-26 00:40:26 +0100

    added https domain

--- Domasno3/frontend/placely-frontend/src/InputCard.js
+++ Domasno3/frontend/placely-frontend/src/InputCard.js
@@ -20,7 +20,7 @@ export default class InputCard extends React.Component {
     }
 
      sendRequest (str)  {
-        fetch('http://206.189.96.78:9090/api/home/userPreferences?'+str, {mode:'cors'})
+        fetch('https://api.placely.tech/api/home/userPreferences?'+str, {mode:'cors'})
             .then((response) => response.json())
             .then((data) => {
                 this.props.onGetResults({"res" : data});
