137707a2cac87aa51e2a6eeb0ae5bda239792243
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-02-09 16:56:14 +0100

    refactored Map into function component

--- Domasno3/frontend/placely-frontend/src/MapComponent.js
+++ Domasno3/frontend/placely-frontend/src/MapComponent.js
@@ -1,6 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import './MapComponent.css'
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useEffect, useMemo } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap, ZoomControl } from 'react-leaflet';
 import L from 'leaflet';
 
@@ -8,97 +8,75 @@ const icon = L.icon({ iconUrl: "/marker-icon.png" });
 const userIcon = L.icon({ iconUrl: "/marker-user-icon.png" });
 
 
-function MyComponent(props) {
+function MapView({ lat, lon }) {
     const map = useMap()
-    if (props.lat !== undefined && props.lon !== undefined)
-        map.setView([props.lat,props.lon], 14)
+    if (lat !== undefined && lon !== undefined)
+        map.setView([lat, lon], 14)
     return null
-  }
+}
 
-  function DraggableMarker(props) {
-    const [position, setPosition] = useState(props.position)
+function DraggableMarker({ position, updatePosition }) {
+    const [positionState, setPosition] = useState(position)
     const markerRef = useRef(null)
     const eventHandlers = useMemo(
         () => ({
             dragend() {
-            const marker = markerRef.current
-            if (marker != null) {
-            setPosition(marker.getLatLng())
-            props.updatePosition(marker.getLatLng().lat, marker.getLatLng().lng)
-        }
-      },
-    }),
-    [],
-  )
+                const marker = markerRef.current
+                if (marker != null) {
+                    setPosition(marker.getLatLng())
+                    updatePosition(marker.getLatLng().lat, marker.getLatLng().lng)
+                }
+            },
+        }),
+        [],
+    )
 
     return (
         <Marker
             draggable={true}
             eventHandlers={eventHandlers}
-            position={position}
+            position={positionState}
             ref={markerRef}
-            icon = {userIcon}
-            autoPan = {true}
-            ></Marker>
+            icon={userIcon}
+            autoPan={true}
+        ></Marker>
     )
-  }
+}
 
-class MapComponent extends React.Component {
+export default function MapComponent({ results, updateLocation1, updateLocation2, pos1, pos2 }) {
+    const { res } = results;
+    const [resState, setRes] = useState(res);
 
-    constructor(props) {
-        super(props);
-        this.state = { res: [] };
-    }
-
-    
-
-    componentDidMount() {
-        const { results } = this.props;
-        this.setState({ res: results.res });
-      }
-    
-      componentDidUpdate(prevProps) {
-        const { results } = this.props;
-        if (results !== prevProps.results) {
-          this.setState({ res: results.res });
+    useEffect(() => {
+        if (results) {
+            setRes(results.res);
         }
-        // var map = useMap();
-        // const res  = this.state.res !== undefined ? this.state.res : [];
-        // if (res.length > 0) {
-        //     map.panTo([res[0].location.latitude, res[0].location.longitude])
-        // }
-    }
-    render() {
-        const { res = [] } = this.state;
-        const first = res.length > 0 ? res[0] : {location: {latitude: 41.996, longitude: 21.4310}};
-    
-        return (
-            
-            <div className='map'>
+    }, [results]);
+
+    const first = (resState != undefined && resState.length > 0) ? resState[0] : { location: { latitude: 41.996, longitude: 21.4310 } };
+
+    return (
+        <div className='map'>
             <MapContainer center={[41.996, 21.4310]} zoom={14} scrollWheelZoom={true} zoomControl={false}>
-
-                <ZoomControl position="bottomright"  />
-                <MyComponent lat={first.location.latitude} lon={first.location.longitude}/>
-
-                
+                <ZoomControl position="bottomright" />
+                <MapView lat={first.location.latitude} lon={first.location.longitude} />
                 <DraggableMarker
-                    position={this.props.pos1}
-                    updatePosition = {this.props.updateLocation1}
+                    position={pos1}
+                    updatePosition={updateLocation1}
                 >
                     <Popup>Location 1</Popup>
                 </DraggableMarker>
                 <DraggableMarker
-                    position={this.props.pos2}
-                    updatePosition = {this.props.updateLocation2}
+                    position={pos2}
+                    updatePosition={updateLocation2}
                 >
                     <Popup>Location 2</Popup>
                 </DraggableMarker>
-
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                
                 {
-                    
-                    res.map((result) => {
+                    resState != undefined ? resState.map((result) => {
                         return (
                             <Marker position={[result.location.latitude, result.location.longitude]} icon={icon}>
                                 <Popup>
@@ -107,13 +85,9 @@ class MapComponent extends React.Component {
                             </Marker>
                         )
                     })
-                
-                } 
-                
+                        : null
+                }
             </MapContainer>
-            </div>
-        )
-    }
-    
+        </div>
+    )
 }
-export default MapComponent;
