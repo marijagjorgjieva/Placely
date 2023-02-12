723539f98a0ca8b3d217fe6cf523aae53ad9e647
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-02-12 17:17:33 +0100

    - refactored Location::calculateDistance - deleted unused files from Prototype

--- Domasno3/backend/src/main/java/mk/finki/ama/dom2/model/Location.java
+++ Domasno3/backend/src/main/java/mk/finki/ama/dom2/model/Location.java
@@ -1,30 +1,38 @@
 package mk.finki.ama.dom2.model;
 
-public class Location {
-    private final double latitude;
-    private final double longitude;
+public record Location(double latitude, double longitude) {
 
-    public Location(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
+    /**
+     * The value 12742 in this code represents the earth's average radius in kilometers.
+     * It is used as a factor to convert the result of the calculation into kilometers.
+     * The calculation in this code computes the Haversine distance,
+     * which is a formula used to find the great-circle distance between two points on a sphere,
+     * such as the earth. The Haversine formula is based on the spherical law of cosines
+     * and uses the earth's average radius to find the distance between two points on the surface of the earth.
+     *
+     * @param first first location
+     * @param second second location
+     * @return the Haversine distance between two locations, first and second, represented in kilometers.
+     */
     public static double calculateDistance(Location first, Location second) {
+        final double earthRadius = 12742;
+        final double radian = Math.PI / 180;
 
-        double p = Math.PI / 2;    // Math.PI / 180
-        double a = 0.5 - Math.cos((second.getLatitude() - first.getLatitude()) * p) / 2 + (Math.cos(first.getLatitude()) * p) * (Math.cos(second.getLatitude()) * p) * (1 - Math.cos((second.getLongitude() - first.getLongitude()) * p)) / 2;
+        double latitudeDiff = (second.latitude() - first.latitude()) * radian;
+        double longitudeDiff = (second.longitude() - first.longitude()) * radian;
 
-        return 12742 * Math.asin(Math.sqrt(a));
+        double latitude1 = first.latitude() * radian;
+        double latitude2 = second.latitude() * radian;
 
+        double a = Math.sin(latitudeDiff / 2) * Math.sin(latitudeDiff / 2)
+                + Math.cos(latitude1) * Math.cos(latitude2)
+                * Math.sin(longitudeDiff / 2) * Math.sin(longitudeDiff / 2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        return earthRadius * c;
     }
+
 
     @Override
     public String toString() {
